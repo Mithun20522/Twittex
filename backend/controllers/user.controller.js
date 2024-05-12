f6138141975f4d067dcd530333d9055d03a29120
mithun20522 <mithun20522@iiitd.ac.in>
2024-05-12 17:25:03 +0530

    update user api route done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -80,3 +80,16 @@ export const getSuggestedUsers = async(req, res) => {
         return res.status(500).json({message:error.message}) 
     }
 }
+
+export const updateUser = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const user = await User.findByIdAndUpdate(id, req.body, {new:true})
+        if(!user){
+            return res.status(404).json({message:'user not found'})
+        }
+        return res.status(200).json({message:'user profile updated'})
+    } catch (error) {
+        return res.status(500).json({message:error.message})
+    }
+}
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { verifyToken } from '../middlewares/verify.middleware.js';
-import { followUnfollowUser, getSuggestedUsers, getUserProfile } from '../controllers/user.controller.js';
+import { followUnfollowUser, getSuggestedUsers, getUserProfile, updateUser } from '../controllers/user.controller.js';
 
 const userRouter = express.Router()
 
 userRouter.get('/get-user-profile/:username', verifyToken, getUserProfile)
 userRouter.get('/follow-unfollow-user/:id', verifyToken, followUnfollowUser)
 userRouter.get('/get-suggested-users', verifyToken, getSuggestedUsers)
+userRouter.patch('/update-user/:id', verifyToken, updateUser)
 
 export default userRouter
