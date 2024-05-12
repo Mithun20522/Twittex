10b123e1e31e819fd63b7d59201b82ef22ff4e06
mithun20522 <mithun20522@iiitd.ac.in>
2024-05-12 16:22:41 +0530

    follow unfollow user api route done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -13,3 +13,35 @@ export const getUserProfile = async(req, res) => {
         return res.status(500).json({message:error.message})
     }
 }
+
+export const followUnfollowUser = async(req, res) => {
+    try {
+        const {id} = req.params
+        const userToModify = await User.findById(id)
+        const currentUser = await User.findById(req.user._id)
+        if(id === req.user._id.toString()){
+            return res.status(400).json({message:"You can't follow/unfollow yourself"})
+        }
+
+        if(!userToModify || !currentUser){
+            return res.status(404).json({message:'No user found'})
+        }
+
+        const isAlreadyFollowing = currentUser.following.includes(id)
+
+        if(isAlreadyFollowing){
+            //unfollow
+            await User.findByIdAndUpdate(id,{$pull: {followers: req.user._id}})
+            await User.findByIdAndUpdate(req.user._id,{$pull: {following: id}})
+            res.status(200).json({message:'User unfollowed successfully'})
+        }
+        else{
+            //follow
+            await User.findByIdAndUpdate(id,{$push: {followers: req.user._id}})
+            await User.findByIdAndUpdate(req.user._id,{$push: {following: id}})
+            res.status(200).json({message:'User followed successfully'})
+        }
+    } catch (error) {
+        return res.status(500).json({message:error.message})
+    }
+}
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { verifyToken } from '../middlewares/verify.middleware.js';
-import { getUserProfile } from '../controllers/user.controller.js';
+import { followUnfollowUser, getUserProfile } from '../controllers/user.controller.js';
 
 const userRouter = express.Router()
 
 userRouter.get('/get-user-profile/:username', verifyToken, getUserProfile)
+userRouter.get('/follow-unfollow-user/:id', verifyToken, followUnfollowUser)
 
 export default userRouter
