92a4d80eeda55835267d7c1c2c1d21c9ecfedfab
mithun20522 <mithun20522@iiitd.ac.in>
2024-05-15 14:35:49 +0530

    delete post api route done

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -30,3 +30,26 @@ export const createPost = async(req, res) => {
         return res.status(500).json({message:error.message})
     }
 }
+
+export const deletePost = async(req, res) => {
+    try {
+        const {id} = req.params
+        const post = await Post.findById(id)
+        if(!post){
+            return res.status(404).json({message:'post not found'})
+        }
+        
+        if(post.user.toString() !== req.user._id.toString()){
+            return res.status(401).json({message:'You are not authorized to delete this post'})
+        }
+        if(post.img){
+            const imgId = post.img.split('/').pop().split('.')[0]
+            await cloudinary.uploader.destroy(imgId)
+        }
+        await Post.findByIdAndDelete(id)
+        return res.status(200).json({message:'post deleted successfully'})
+        
+    } catch (error) {
+        return res.status(500).json({message:error.message})
+    }
+}
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { verifyToken } from '../middlewares/verify.middleware.js';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, deletePost } from '../controllers/post.controller.js';
 
 const postRouter = express.Router()
 
 postRouter.post('/create-post',verifyToken, createPost)
+postRouter.delete('/delete-post/:id', verifyToken, deletePost)
 
 export default postRouter
