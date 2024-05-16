e793738f6acec3063625edb28e81b0458ddbb9cf
mithun20522 <mithun20522@iiitd.ac.in>
2024-05-16 21:14:24 +0530

    page not found UI done

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <main class="h-screen w-full flex flex-col justify-center items-center">
+	<h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
+	<div class="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
+		Page Not Found
+	</div>
+	<button class="mt-5">
+      <a
+        class="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
+      >
+      <Link className='btn btn-primary btn-outline text-white rounded-lg w-full' to="/">Go Home</Link>
+      </a>
+    </button>
+</main>
   )
 }
 
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import CreatePost from '../home/CreatePost'
+import Posts from '../../components/common/Posts';
 const Home = () => {
+  const [feedType, setFeedType] = useState('foryou')
   return (
-    <div>Home</div>
+    <>
+      <div className='flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen'>
+        <div className='flex w-full border-b border-gray-700'>
+          <div 
+          className='flex flex-1 justify-center p-3 hover:bg-secondary cursor-pointer transition
+          duration-300 relative'
+          onClick={() => setFeedType('foryou')}>
+            For you
+            {
+              feedType === 'foryou' && (
+                <div className='bg-primary w-10 h-1 absolute bottom-0 rounded-full'></div>
+              )
+            }
+            </div>
+          <div
+          className='flex flex-1 justify-center p-3 hover:bg-secondary cursor-pointer transition
+          duration-300 relative'
+          onClick={() => setFeedType('following')}>
+            Following
+            {
+              feedType === 'following' && (
+                <div className='bg-primary w-10 h-1 absolute bottom-0 rounded-full'></div>
+              )
+            }
+            </div>
+        </div>
+        <CreatePost/>
+        <Posts/>
+      </div>
+    </>
   )
 }
 
